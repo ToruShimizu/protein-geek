9315faa004d00aff226148b7e6c8aa78e8282fd7
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-07-07 19:48:41 +0900

    chore: modern-css-resetを追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
+import 'modern-css-reset/dist/reset.min.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
