a5f850741cd3ebc189ade418fbe65f109654407a
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-07-08 01:37:29 +0900

    Headerコンポーネントを追加 (#21)

---
+++ app/components/header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <div>
+      <header className="p-4 md:p-6">
+        <h1 className="justify-center flex font-extrabold text-xl md:text-3xl">
+          <Link href="/">プロテインザスター</Link>
+        </h1>
+      </header>
+      <hr className="border-1" />
+    </div>
+  )
+}
