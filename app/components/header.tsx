0704388236de3a497561cfe64c91eeee5b05d3cb
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-07-08 02:02:13 +0900

    feat: footerコンポーネントを追加

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div>
       <header className="p-4 md:p-6">
         <h1 className="justify-center flex font-extrabold text-xl md:text-3xl">
-          <Link href="/">プロテインザスター</Link>
+          <Link href="/">Protein The Star</Link>
         </h1>
       </header>
       <hr className="border-1" />
