3fc48163248bc227713a1efbb32958efa294c4ab
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-07-07 20:50:44 +0900

    開発前の整理 (#16)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+
+import 'modern-css-reset/dist/reset.min.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
