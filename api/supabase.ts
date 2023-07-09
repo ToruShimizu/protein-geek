fa4f41e102c6615aa2608c92eb27ed502803993f
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-07-09 19:26:49 +0900

    GraphQLコードの生成方法を変更 (#28)

--- api/supabase.ts
+++ api/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js"
 import { Database } from "../types/supabase"
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? ""
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY ?? ""
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY)
