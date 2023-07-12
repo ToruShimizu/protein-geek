2366808f5250cf70af515882b82b21ded54e0d72
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-07-13 02:31:16 +0900

    feat: プロテイン一覧画面を実装

--- app/makers/[id]/page.tsx
+++ app/makers/[id]/page.tsx
@@ -1,13 +1,35 @@
 import { proteinRepo } from "../../../repos/proteins"
+import { makerRepo } from "../../../repos/makers"
+import Link from "next/link"
 
 export default async function Page({ params }: { params: { id: string } }) {
-  // eslint-disable-next-line
+  const maker = await makerRepo.fetchById(Number(params.id))
   const proteins = await proteinRepo.fetchByMakerId(Number(params.id))
 
   return (
     <main>
       <section>
-        <h2 className="text-3xl font-bold pb-6">MY PROTEIN</h2>
+        <h2 className="text-3xl font-bold pb-6">{maker.name}</h2>
+        <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-6">
+          {proteins.map(({ node }) => (
+            <li key={node.id} className="grid gap-1 md:gap-2">
+              <Link href={`/makers/${maker.id}/proteins/${node.id}`}>
+                <div className="grid gap-2 group">
+                  {node.src && (
+                    <div className="relative">
+                      <img src={node.src} alt="プロテインメーカー" />
+                      <div className="absolute bottom-0 left-0 right-0 top-0 bg-stone-800 opacity-0 transition duration-300 ease-in-out group-hover:opacity-50"></div>
+                    </div>
+                  )}
+                  <div>
+                    <p className="mb-1 text-xs lg:text-sm font-bold text-gray-400">{node.name}</p>
+                    <p className="text-sm md:text-base font-bold text-zinc-600">{node.flavor}</p>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </section>
     </main>
   )
