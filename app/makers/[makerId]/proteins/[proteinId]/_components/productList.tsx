1356cab12f46837d8f491711a6da71ef48549baa
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-07-29 17:02:09 +0900

    refactor: 容量一覧をコンポーネント化

---
+++ app/makers/[makerId]/proteins/[proteinId]/_components/productList.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { ProteinIdResponse } from "../../../../../../types/responses"
+
+type Props = {
+  products: ProteinIdResponse["products"]
+}
+
+export default function ProductList({ products }: Props) {
+  return (
+    <ul className="grid grid-cols-2 gap-4">
+      {products.map((product) => (
+        <li
+          key={product.id}
+          className="p-2 bg-white border border-gray-200 rounded-lg shadow cursor-pointer"
+        >
+          {product.capacity}
+        </li>
+      ))}
+    </ul>
+  )
+}
