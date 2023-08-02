3e374e2309cc769f9c13f1b0bb1b3050f9a3298e
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-08-03 02:27:34 +0900

    パンくずリストを各ページに追加 (#106)

--- app/_components/breadcrumbs.tsx
+++ app/_components/breadcrumbs.tsx
@@ -1,8 +1,10 @@
 "use client"
 import Link from "next/link"
+import { montSerrat } from "../_styles/fonts"
 
 type Breadcrumb = {
   name: string
+  icon?: JSX.Element
   href?: string
 }
 
@@ -11,46 +13,39 @@ type Props = {
 }
 
 export default function Breadcrumbs({ lists }: Props) {
+  const baseTextStyle = `text-sm md:text-base font-medium text-gray-700 
+  text-gray-700 ${montSerrat.className}`
   return (
-    <nav className="p-3 md:p-6" aria-label="Breadcrumb">
-      <ol className="flex">
-        {lists.map(({ name, href }, index) => (
-          <>
-            {index !== 0 && (
-              <div className="flex items-center">
-                <svg
-                  className="w-3 h-3 text-gray-400 mx-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-              </div>
+    <nav className="p-3 md:p-6 overflow-x-auto" aria-label="Breadcrumb">
+      <ol className="grid auto-cols-max grid-flow-col gap-1">
+        {lists.map(({ name, href, icon }, index) => (
+          <li key={name} className="grid auto-cols-max grid-flow-col items-center">
+            {index > 0 && (
+              <svg
+                className="w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
             )}
 
-            <li key={name} className="grid items-center">
-              {href ? (
-                <Link
-                  href={href}
-                  className="inline-flex items-center text-sm md:text-base font-medium  text-gray-400 hover:underline"
-                >
-                  {name}
-                </Link>
-              ) : (
-                <span className="text-sm md:text-base font-medium  md:ml-2 text-gray-400">
-                  {name}
-                </span>
-              )}
-            </li>
-          </>
+            {href ? (
+              <Link href={href} className={`hover:underline hover:opacity-75 ${baseTextStyle}`}>
+                {icon ?? name}
+              </Link>
+            ) : (
+              <span className={baseTextStyle}>{icon ?? name}</span>
+            )}
+          </li>
         ))}
       </ol>
     </nav>
--- app/makers/[makerId]/proteins/[proteinId]/page.tsx
+++ app/makers/[makerId]/proteins/[proteinId]/page.tsx
@@ -1,14 +1,29 @@
+import { makerRepo } from "../../../../../repos/makers"
 import { proteinRepo } from "../../../../../repos/proteins"
 import { reviewRepo } from "../../../../../repos/reviews"
+import Breadcrumbs from "../../../../_components/breadcrumbs"
+import HomeIcon from "../../../../_components/icons/homeIcon"
 import ProteinContainer from "./_components/proteinContainer"
 
-export default async function Page({ params }: { params: { proteinId: string } }) {
-  const { protein } = await proteinRepo.fetchById(Number(params.proteinId))
+export default async function Page({ params }: { params: { makerId: string; proteinId: string } }) {
+  const [{ protein }, { maker }] = await Promise.all([
+    proteinRepo.fetchById(Number(params.proteinId)),
+    makerRepo.fetchById(Number(params.makerId)),
+  ])
+
   const flavorIds = protein.flavors.map((flavor) => flavor.id)
   const reviews = await reviewRepo.fetchByFlavorIds(flavorIds)
 
   return (
     <main className="grid gap-16 md:gap-20">
+      <Breadcrumbs
+        lists={[
+          { name: "top", icon: <HomeIcon />, href: "/" },
+          { name: "メーカー一覧", href: "/makers" },
+          { name: maker.name, href: `/makers/${maker.id}` },
+          { name: protein.name },
+        ]}
+      />
       <ProteinContainer protein={protein} reviews={reviews} />
     </main>
   )
