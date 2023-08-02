946db6562db3d2f4925bd4c3a82f1640fa6af08d
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-08-03 00:57:34 +0900

    feat: pageのテキストのフォントを変更

--- app/makers/[makerId]/page.tsx
+++ app/makers/[makerId]/page.tsx
@@ -1,6 +1,7 @@
 import { makerRepo } from "../../../repos/makers"
 import { Suspense } from "react"
 import ProteinList from "./_components/proteinList"
+import { montSerrat } from "../../_styles/fonts"
 
 export default async function Page({ params }: { params: { makerId: string } }) {
   const { maker, proteins } = await makerRepo.fetchById(Number(params.makerId))
@@ -8,7 +9,7 @@ export default async function Page({ params }: { params: { makerId: string } }) 
   return (
     <main>
       <section>
-        <h2 className="text-3xl font-bold pb-6">{maker.name}</h2>
+        <h2 className={`text-3xl font-bold pb-6 ${montSerrat.className}`}>{maker.name}</h2>
         <Suspense fallback={<div>...loading</div>}>
           {proteins && <ProteinList proteins={proteins} />}
         </Suspense>
