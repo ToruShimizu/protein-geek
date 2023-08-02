0664cfa11d64fc1464f8d7bb15626b75d23f6b01
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-08-02 13:56:05 +0900

    レビュー一覧取得のつなぎこみ (#100)

--- app/makers/[makerId]/proteins/[proteinId]/_components/reviewSection.tsx
+++ app/makers/[makerId]/proteins/[proteinId]/_components/reviewSection.tsx
@@ -1,28 +1,28 @@
 "use client"
+import { useMemo } from "react"
+import { Flavor, Review } from "../../../../../../types/responses"
 import ReviewCards from "../../../../../_components/reviewCards"
 import ReviewForm from "../../../../../_components/reviewForm"
 
 type Props = {
-  reviews: {
-    id: number
-    description: string
-    name: string
-    rate: number
-    title: string
-  }[]
-  flavorId: number
+  flavor: Flavor
+  reviews?: Review[]
 }
-export default function ReviewSection({ reviews, flavorId }: Props) {
+export default function ReviewSection({ flavor, reviews }: Props) {
+  const filteredReviews = useMemo(
+    () => reviews?.filter((review) => review.flavor_id === flavor.id),
+    [reviews, flavor],
+  )
   return (
     <section className="grid gap-8">
       <div>
         <h2 className="text-lg lg:text-xl xl:text-2xl font-bold mb-2 md:mb-4">
-          選択された味のレビュー
+          {flavor.name}のレビュー
         </h2>
         <hr className="border-1" />
       </div>
-      <ReviewForm flavorId={flavorId} />
-      <ReviewCards reviews={reviews} />
+      <ReviewForm flavorId={flavor.id} />
+      <ReviewCards reviews={filteredReviews} />
     </section>
   )
 }
