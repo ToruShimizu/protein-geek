9badb581e2b1b5df7a4ad2677066b43c5aa04b0a
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-08-03 02:08:28 +0900

    fix: パンくずリストのデザインを修正

--- app/_components/breadcrumbs.tsx
+++ app/_components/breadcrumbs.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Link from "next/link"
+import { montSerrat } from "../_styles/fonts"
 
 type Breadcrumb = {
   name: string
@@ -12,46 +13,39 @@ type Props = {
 }
 
 export default function Breadcrumbs({ lists }: Props) {
+  const baseTextStyle = `text-sm md:text-base font-medium text-gray-700 
+  text-gray-700 ${montSerrat.className}`
   return (
-    <nav className="p-3 md:p-6" aria-label="Breadcrumb">
-      <ol className="flex">
+    <nav className="p-3 md:p-6 overflow-x-auto" aria-label="Breadcrumb">
+      <ol className="grid auto-cols-max grid-flow-col gap-1">
         {lists.map(({ name, href, icon }, index) => (
-          <>
-            {index !== 0 && (
-              <div className="flex items-center">
-                <svg
-                  className="w-3 h-3 text-gray-400 mx-1"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 6 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="m1 9 4-4-4-4"
-                  />
-                </svg>
-              </div>
+          <li key={name} className="grid auto-cols-max grid-flow-col items-center">
+            {index > 0 && (
+              <svg
+                className="w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
             )}
 
-            <li key={name} className="grid items-center">
-              {href ? (
-                <Link
-                  href={href}
-                  className="inline-flex items-center text-sm md:text-base font-medium  text-gray-400 hover:underline"
-                >
-                  {icon ?? name}
-                </Link>
-              ) : (
-                <span className="text-sm md:text-base font-medium  md:ml-2 text-gray-400">
-                  {icon ?? name}
-                </span>
-              )}
-            </li>
-          </>
+            {href ? (
+              <Link href={href} className={`hover:underline hover:opacity-75 ${baseTextStyle}`}>
+                {icon ?? name}
+              </Link>
+            ) : (
+              <span className={baseTextStyle}>{icon ?? name}</span>
+            )}
+          </li>
         ))}
       </ol>
     </nav>
