1ec34873d84ebd415e5797a7cdf69753176ce9c2
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-08-02 09:21:29 +0900

    feat: クライアント側でレビュー一覧を取得する処理を追加

--- types/responses.d.ts
+++ types/responses.d.ts
@@ -1,9 +1,11 @@
 import { clientFactRepo } from "../repos/facts"
 import { proteinRepo } from "../repos/proteins"
+import { clientReviewRepo } from "../repos/reviews"
 import { UnWrapArray } from "./utils"
 
 export type ProteinIdResponse = Awaited<ReturnType<typeof proteinRepo.fetchById>>
 export type FactByProteinIdResponse = ReturnType<typeof clientFactRepo.fetchByProteinId>
+export type ReviewByFlavorIdResponse = ReturnType<typeof clientReviewRepo.fetchByFlavorIds>
 export type Protein = ProteinIdResponse["protein"]
 export type Fact = FactByProteinIdResponse["fact"]
 export type Nutrient = UnWrapArray<Fact["nutrients"]>
@@ -12,4 +14,5 @@ export type Fact = ProteinIdResponse["fact"]
 export type Flavor = UnWrapArray<Protein["flavors"]>
 export type Product = UnWrapArray<Flavor["products"]>
 export type Seller = Flavor["seller"]
+export type Review = UnWrapArray<ReviewByFlavorIdResponse>
 export type ShopKey = Extract<keyof Seller, "amazon" | "rakuten" | "yahoo" | "official">
