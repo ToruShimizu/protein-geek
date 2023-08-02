d440bf74f5c600208888cecef4d5c6ab337cc3a9
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-08-03 00:56:29 +0900

    feat: メインで使う4種類のフォントを追加

---
+++ app/_styles/fonts.ts
@@ -0,0 +1,29 @@
+import { Chokokutai, Montserrat, Noto_Sans_JP, Varela_Round } from "@next/font/google"
+
+/**
+ * サイト名
+ */
+export const chokokutai = Chokokutai({ weight: "400", subsets: ["latin"], display: "swap" })
+
+/**
+ * 主に英語表記で使うが、数字を太字で表現したい場合は数字でも使う
+ */
+export const montSerrat = Montserrat({
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+})
+
+/**
+ * 日本語表記
+ */
+export const notoSansJp = Noto_Sans_JP({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  display: "swap",
+})
+
+/**
+ * 数字で使うが、数字を太文字で表現したいときには`montSerrat`を使う
+ */
+export const varelaRound = Varela_Round({ weight: "400", subsets: ["latin"], display: "swap" })
