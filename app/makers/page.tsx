4caaaa7183538735b5bdf8fa1e83456f1c7a0dda
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-08-03 23:40:52 +0900

    refactor: mainタグでラップできるコンポーネントを作成

--- app/makers/page.tsx
+++ app/makers/page.tsx
@@ -1,6 +1,6 @@
 import { makerRepo } from "../../repos/makers"
-import Breadcrumbs from "../_components/breadcrumbs"
 import HomeIcon from "../_components/icons/homeIcon"
+import MainContainer from "../_components/mainContainer"
 import { montSerrat } from "../_styles/fonts"
 import MakerList from "./_components/makerList"
 import { Suspense } from "react"
@@ -9,16 +9,15 @@ export default async function Page() {
   const makers = await makerRepo.fetch()
 
   return (
-    <main>
-      <Breadcrumbs
-        lists={[{ name: "top", icon: <HomeIcon />, href: "/" }, { name: "メーカー一覧" }]}
-      />
+    <MainContainer
+      lists={[{ name: "top", icon: <HomeIcon />, href: "/" }, { name: "メーカー一覧" }]}
+    >
       <section>
         <h2 className={`text-3xl font-bold pb-6 ${montSerrat.className}`}>メーカー一覧</h2>
         <Suspense fallback={<div>...loading</div>}>
           <MakerList makers={makers} />
         </Suspense>
       </section>
-    </main>
+    </MainContainer>
   )
 }
