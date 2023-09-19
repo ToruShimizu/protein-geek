0eba76bb8d7e3f40b61979a6a42a8a46a405e633
ToruShimizu <nobbyz.t.nn@gmail.com>
2023-09-20 01:10:33 +0900

    fix: imgををnext/imgに変更

--- app/makers/[makerId]/_components/proteinList.tsx
+++ app/makers/[makerId]/_components/proteinList.tsx
@@ -6,6 +6,7 @@ import { montSerrat } from "@/app/_styles/fonts"
 import { staticUrl } from "_constants/urls"
 import { calculateRateAverage, createStaticUrl } from "modules/utils"
 import Link from "next/link"
+import Image from "next/image"
 import { ProteinByMakerIdResponse } from "types/responses"
 
 type Props = {
@@ -16,6 +17,8 @@ type Props = {
 }
 
 export default function ProteinList({ proteins }: Props) {
+  console.log(proteins[0].src)
+
   return (
     <UnorderedList>
       {proteins.map(({ id, src, name, maker_id, reviews }) => (
@@ -24,12 +27,16 @@ export default function ProteinList({ proteins }: Props) {
             <div className="grid gap-2 group">
               {src && (
                 <div className="relative">
-                  <img
+                  <Image
+                    width={0}
+                    height={0}
+                    sizes="100vw"
                     src={createStaticUrl({
                       baseUrl: staticUrl,
                       src,
                     })}
                     alt={name}
+                    style={{ width: "100%", height: "100%" }}
                   />
                   <div className="absolute bottom-0 left-0 right-0 top-0 bg-stone-800 opacity-0 transition duration-300 ease-in-out group-hover:opacity-50"></div>
                 </div>
