2bc9b60595da75016837a8f300d99dd112164c9e
toru_shimizu <65491855+ToruShimizu@users.noreply.github.com>
2023-09-20 00:55:15 +0900

    Tailwind Elementsの考慮漏れを修正 (#163)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/app/_components/header"
 import Footer from "@/app/_components/footer"
+import "tw-elements/dist/css/tw-elements.min.css"
 
 import "@/app/_styles/reset.css"
 import "@/app/_styles/globals.css"
