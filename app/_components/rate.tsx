626830c7f2ac2509bffb27cb4e6bece56e659b75
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-01-28 14:14:35 +0900

    fix: スターの大きさを修正

--- app/_components/rate.tsx
+++ app/_components/rate.tsx
@@ -18,7 +18,7 @@ export default function Rate({ rate, count }: Rate) {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
                 fill={i + 1 <= rate ? "currentColor" : "none"}
-                className="mr-1 h-5 w-5 text-yellow-300"
+                className="mr-1 h-4 w-4 text-yellow-300"
               >
                 <path
                   fillRule="evenodd"
