c27f1e0029e77959fd78291d927f3624b70efd82
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-01-29 23:19:24 +0900

    fix: タイトルを変更

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link"
-import { reggaeOne } from "@/app/_styles/fonts"
+import { oswald } from "@/app/_styles/fonts"
 
 export default function Header() {
   return (
     <div>
       <header className="p-4 md:p-6">
         <h1
-          className={`justify-center flex font-extrabold text-xl md:text-3xl ${reggaeOne.className}`}
+          className={`justify-center flex font-extrabold text-xl md:text-3xl ${oswald.className}`}
         >
-          <Link href="/">プロテインザスター</Link>
+          <Link href="/">リアルプロテイン</Link>
         </h1>
       </header>
       <hr className="border-1" />
