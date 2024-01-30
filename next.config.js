c08747313f2d19337a48ae269c533e609c16c6d3
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-01-30 22:29:09 +0900

    fix: appDirの指定は不要

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const SUPABASE_DOMAIN = process.env.NEXT_PUBLIC_SUPABASE_DOMAIN
 
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: [SUPABASE_DOMAIN],
   },
