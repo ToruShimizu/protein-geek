a55872e449921d6692d39cc955668e7aa0b6fe32
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-02-03 00:05:57 +0900

    fix: purgeは不要だった

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./app/**/*.{js,ts,jsx,tsx,mdx,css}", "./node_modules/tw-elements-react/dist/js/**/*.js"],
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/tw-elements-react/dist/js/**/*.js"],
   theme: {
     extend: {},
