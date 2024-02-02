49125e69092438259502ecf2f8fdff5d2c62e8ad
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-02-02 23:58:50 +0900

    fix: 未使用のcssを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./app/**/*.{js,ts,jsx,tsx,mdx,css}", "./node_modules/tw-elements-react/dist/js/**/*.js"],
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/tw-elements-react/dist/js/**/*.js"],
   theme: {
     extend: {},
