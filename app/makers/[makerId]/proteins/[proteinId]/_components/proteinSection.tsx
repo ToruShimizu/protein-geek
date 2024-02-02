3a62a04ae68f5eca99eecc0bbd0be5fc2f783e68
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-02-02 22:11:46 +0900

    fix: スクロールなしで表示される画像の遅延読み込みをやめる

--- app/makers/[makerId]/proteins/[proteinId]/_components/proteinSection.tsx
+++ app/makers/[makerId]/proteins/[proteinId]/_components/proteinSection.tsx
@@ -69,6 +69,7 @@ export default function ProteinSection({ protein }: Props) {
             src: protein.src,
           })}
           alt={protein.name}
+          loading="eager"
           style={{ width: "100%", height: "100%" }}
         />
       </div>
