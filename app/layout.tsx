b7b208d35071cdc961500fc852abe8429ac272a8
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-02-02 23:33:07 +0900

    fix: tw-elementをレイアウトではインポートしない

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import Header from "@/app/_components/header"
 import Footer from "@/app/_components/footer"
-import "tw-elements-react/dist/css/tw-elements-react.min.css"
 
 import "@/app/_styles/reset.css"
 import "@/app/_styles/globals.css"
