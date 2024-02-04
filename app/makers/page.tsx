4ffae1780d1634e6745e002dd5b4394a51ff8819
ToruShimizu <nobbyz.t.nn@gmail.com>
2024-02-04 15:15:57 +0900

    fix: 準備中に変更

--- app/makers/page.tsx
+++ app/makers/page.tsx
@@ -4,17 +4,20 @@ import MainContainer from "@/app/_components/mainContainer"
 import PageTitle from "@/app/_components/pageTitle"
 import MakerList from "@/app/makers/_components/makerList"
 
+export const revalidate = 0
+
 export default async function Page() {
   const makers = await makerRepo.fetch()
 
   return (
-    <MainContainer
-      lists={[{ name: "top", icon: <HomeIcon />, href: "/" }, { name: "メーカー一覧" }]}
-    >
-      <section>
-        <PageTitle>メーカー一覧</PageTitle>
-        <MakerList makers={makers} />
-      </section>
-    </MainContainer>
+    <div>準備中</div>
+    // <MainContainer
+    //   lists={[{ name: "top", icon: <HomeIcon />, href: "/" }, { name: "メーカー一覧" }]}
+    // >
+    //   <section>
+    //     <PageTitle>メーカー一覧</PageTitle>
+    //     <MakerList makers={makers} />
+    //   </section>
+    // </MainContainer>
   )
 }
